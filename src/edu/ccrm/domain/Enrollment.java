3e10ba74b3229da9d9519d944de8b93264d840d2
Eshaan2605 <eshaanabrol@gmail.com>
2026-09-17 21:34:22 +0530

    feat: add semester and drop support to Enrollment

--- src/edu/ccrm/domain/Enrollment.java
+++ src/edu/ccrm/domain/Enrollment.java
@@ -2,22 +2,35 @@ package edu.ccrm.domain;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents a student's enrollment in a course for a given semester.
+ */
 public class Enrollment {
     private final String courseCode;
     private final int courseCredits;
     private final LocalDateTime enrolledAt;
     private Grade grade;
+    private Semester semester;
+    private boolean dropped;
 
     public Enrollment(String courseCode, int credits){
         this.courseCode = courseCode;
         this.courseCredits = credits;
         this.enrolledAt = LocalDateTime.now();
+        this.dropped = false;
     }
     public String getCourseCode(){ return courseCode; }
     public int getCourseCredits(){ return courseCredits; }
     public LocalDateTime getEnrolledAt(){ return enrolledAt; }
     public Grade getGrade(){ return grade; }
     public void setGrade(Grade g){ this.grade = g; }
+    public Semester getSemester(){ return semester; }
+    public void setSemester(Semester s){ this.semester = s; }
+    public boolean isDropped(){ return dropped; }
+    public void drop(){ this.dropped = true; }
+
     @Override
-    public String toString(){ return String.format("Enrollment[%s,%dcr,grade=%s]", courseCode, courseCredits, grade); }
+    public String toString(){
+        return String.format("Enrollment[%s,%dcr,grade=%s,dropped=%s]", courseCode, courseCredits, grade, dropped);
+    }
 }
