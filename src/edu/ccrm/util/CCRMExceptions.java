8c031b6ba9977eea487dd43f54302016dcc269c0
Eshaan2605 <eshaanabrol@gmail.com>
2026-09-17 21:34:22 +0530

    feat: add StudentNotFoundException, CourseNotFoundException, InvalidGradeException

--- src/edu/ccrm/util/CCRMExceptions.java
+++ src/edu/ccrm/util/CCRMExceptions.java
@@ -1,10 +1,22 @@
 package edu.ccrm.util;
 
+/**
+ * Custom exception hierarchy for CCRM application.
+ */
 public class CCRMExceptions {
     public static class DuplicateEnrollmentException extends Exception {
         public DuplicateEnrollmentException(String msg){ super(msg); }
     }
     public static class MaxCreditLimitExceededException extends Exception {
         public MaxCreditLimitExceededException(String msg){ super(msg); }
+    }
+    public static class StudentNotFoundException extends RuntimeException {
+        public StudentNotFoundException(String id){ super("Student not found: " + id); }
+    }
+    public static class CourseNotFoundException extends RuntimeException {
+        public CourseNotFoundException(String code){ super("Course not found: " + code); }
+    }
+    public static class InvalidGradeException extends RuntimeException {
+        public InvalidGradeException(String msg){ super(msg); }
     }
 }
