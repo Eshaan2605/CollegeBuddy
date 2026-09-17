f1eb2bac7c4aad58109184c2187dce8d0b084bf9
Eshaan2605 <eshaanabrol@gmail.com>
2026-09-17 21:34:23 +0530

    feat: add listAllFiles and countFiles to RecursiveUtil

--- src/edu/ccrm/util/RecursiveUtil.java
+++ src/edu/ccrm/util/RecursiveUtil.java
@@ -3,9 +3,15 @@ package edu.ccrm.util;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Utility class for recursive file system operations.
+ */
 public class RecursiveUtil {
-    // Recursively compute total size of directory (bytes) using NIO
+
+    /** Recursively compute total size of directory in bytes. */
     public static long computeSize(Path root) throws IOException {
         final long[] total = {0};
         Files.walkFileTree(root, new SimpleFileVisitor<Path>(){
@@ -16,5 +22,31 @@ public class RecursiveUtil {
             }
         });
         return total[0];
+    }
+
+    /** Recursively list all files under a directory. */
+    public static List<Path> listAllFiles(Path root) throws IOException {
+        List<Path> result = new ArrayList<>();
+        Files.walkFileTree(root, new SimpleFileVisitor<Path>(){
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs){
+                result.add(file);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        return result;
+    }
+
+    /** Count number of files recursively. */
+    public static long countFiles(Path root) throws IOException {
+        final long[] count = {0};
+        Files.walkFileTree(root, new SimpleFileVisitor<Path>(){
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs){
+                count[0]++;
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        return count[0];
     }
 }
