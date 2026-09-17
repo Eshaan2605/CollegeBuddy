99db07b21ddad4d1885a0d3d61d7df6a91b570df
Eshaan2605 <eshaanabrol@gmail.com>
2026-09-17 21:40:25 +0530

    chore: clean up repo structure and update README

--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Compiling CollegeBuddy...
-if exist out rmdir /s /q out
-mkdir out
-for /r src %%f in (*.java) do javac -d out "%%f"
-echo Running CollegeBuddy...
-java -cp out edu.ccrm.cli.Main
