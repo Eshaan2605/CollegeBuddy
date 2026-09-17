385ba189ea6677b06fa4d7d2e8930ea5129c8fb5
Eshaan2605 <eshaanabrol@gmail.com>
2026-09-17 21:34:22 +0530

    docs: add Javadoc and isMainTerm() to Semester enum

--- src/edu/ccrm/domain/Semester.java
+++ src/edu/ccrm/domain/Semester.java
@@ -1,3 +1,19 @@
 package edu.ccrm.domain;
 
-public enum Semester { SPRING, SUMMER, FALL }
+/**
+ * Academic semester designations used across the system.
+ */
+public enum Semester {
+    /** January-April term */
+    SPRING,
+    /** May-August term */
+    SUMMER,
+    /** September-December term */
+    FALL,
+    /** Full academic year */
+    ANNUAL;
+
+    public boolean isMainTerm(){
+        return this == SPRING || this == FALL;
+    }
+}
