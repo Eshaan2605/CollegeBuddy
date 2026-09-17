6de4bfba284cef60c58d42e20ead051b978a3253
Eshaan2605 <eshaanabrol@gmail.com>
2026-09-17 21:34:21 +0530

    feat: implement Instructor domain class with department and designation

--- src/edu/ccrm/domain/Instructor.java
+++ src/edu/ccrm/domain/Instructor.java
@@ -1,12 +1,27 @@
 package edu.ccrm.domain;
 
+/**
+ * Represents an instructor/faculty member.
+ */
 public class Instructor extends Person {
     private String department;
+    private String designation;
+
     public Instructor(String id, String fullName, String email, String department){
         super(id, fullName, email);
         this.department = department;
+        this.designation = "Assistant Professor";
     }
     public String getDepartment(){ return department; }
+    public void setDepartment(String d){ this.department = d; }
+    public String getDesignation(){ return designation; }
+    public void setDesignation(String d){ this.designation = d; }
+
     @Override
     public String role(){ return "Instructor"; }
+
+    @Override
+    public String toString(){
+        return String.format("Instructor[id=%s,name=%s,dept=%s,designation=%s]", id, fullName, department, designation);
+    }
 }
