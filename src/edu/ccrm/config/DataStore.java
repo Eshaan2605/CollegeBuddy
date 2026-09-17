0cda26df7767fdaeff8dc01e24b7488a8b45cad2
Eshaan2605 <eshaanabrol@gmail.com>
2026-09-17 21:54:19 +0530

    feat: update course data - new instructors, add CSE3004 Computer Networks, remove CSE3001

--- src/edu/ccrm/config/DataStore.java
+++ src/edu/ccrm/config/DataStore.java
@@ -38,21 +38,22 @@ public class DataStore {
         students.put(s2.getId(), s2);
         students.put(s3.getId(), s3);
 
-        Instructor i1 = new Instructor("INS001","Dr. Irfan Alam","irfan@vit.ac.in","Computer Science");
-        Instructor i2 = new Instructor("INS002","Dr. Sanat Jain","sanat@vit.ac.in","Computer Science");
+        Instructor i1 = new Instructor("INS001","Dr. Vipin Jain","vipin@vit.ac.in","Computer Science");
+        Instructor i2 = new Instructor("INS002","Dr. Ram Kumar","ramkumar@vit.ac.in","Computer Science");
+        Instructor i3 = new Instructor("INS003","Dr. Ramesh Babu","ramesh@vit.ac.in","Computer Science");
+        Instructor i4 = new Instructor("INS004","Dr. Amit Kumar Singh","amit@vit.ac.in","Electronics");
         instructors.put(i1.getId(), i1);
         instructors.put(i2.getId(), i2);
+        instructors.put(i3.getId(), i3);
+        instructors.put(i4.getId(), i4);
 
-        Course c1 = new Course.Builder("CSE3001").title("Database Management System").credits(4).instructor("Dr. Irfan Alam").semester(Semester.FALL).department("Computer Science").build();
-        Course c2 = new Course.Builder("CSE3002").title("Programming in Java").credits(3).instructor("Dr. Sanat Jain").semester(Semester.FALL).department("Computer Science").build();
-        Course c3 = new Course.Builder("CSE3003").title("Data Structures and Algorithms").credits(4).instructor("Dr. Irfan Alam").semester(Semester.FALL).department("Computer Science").build();
+        Course c1 = new Course.Builder("CSE3002").title("Programming in Java").credits(3).instructor("Dr. Vipin Jain").semester(Semester.FALL).department("Computer Science").build();
+        Course c2 = new Course.Builder("CSE3003").title("Data Structures and Algorithms").credits(4).instructor("Dr. Ram Kumar").semester(Semester.FALL).department("Computer Science").build();
+        Course c3 = new Course.Builder("CSE3004").title("Computer Networks").credits(3).instructor("Dr. Ramesh Babu").semester(Semester.FALL).department("Computer Science").build();
+        Course c4 = new Course.Builder("ECE3001").title("Signals and Systems").credits(3).instructor("Dr. Amit Kumar Singh").semester(Semester.FALL).department("Electronics").build();
         courses.put(c1.getCode(), c1);
         courses.put(c2.getCode(), c2);
-        Course c4 = new Course.Builder("ECE3001").title("Signals and Systems").credits(3).instructor("Dr. Meena Rao").semester(Semester.FALL).department("Electronics").build();
-        courses.put(c4.getCode(), c4);
         courses.put(c3.getCode(), c3);
+        courses.put(c4.getCode(), c4);
     }
 }
-
-
-
